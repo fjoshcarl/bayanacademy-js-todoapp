d27a56dc920fb750a25ae7d1f6b9e0da901f2946
Josh Francisco <fjoshcarl@yahoo.com>
2023-03-26 02:22:55 +0800

    Create tailwind.config.js
    Initialize tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          americansilver: '#D1D1D1',
+          darkcharcoal: '#313131',
+          internationalorange: '#C0392B',
+        }
+      }
+    }
+  }
